f24fd3d39e0638df8073c806471eaf95266675bc
sunxueqi <1090650726@qq.com>
2022-01-15 12:13:48 +0800

    init project

---
+++ app/src/main/java/com/sxq/weatherdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sxq.weatherdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
